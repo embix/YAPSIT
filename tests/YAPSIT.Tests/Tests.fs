1ed59c674152e29bc0a02232d74a0938a1d4e3d4
Michel Bretschneider <mbretsch@questnet.de>
2017-11-01 15:58:42 +0100

    as initialized with project scaffold for 'YAPSIT'

---
+++ tests/YAPSIT.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module YAPSIT.Tests
+
+open YAPSIT
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
